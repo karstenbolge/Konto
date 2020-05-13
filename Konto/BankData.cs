714cf38add00e62bfbf31f6b4a04ebf7fb1f555e
Karsten Bolge <karstenbolge@yahoo.dk>
2020-05-13 12:06:23 +0200

    fixed

--- Konto/BankData.cs
+++ Konto/BankData.cs
@@ -33,23 +33,7 @@ namespace Converter
 
             if (lines.Length > 1)
             {
-                int ub = 0;
-                int kr = 0;
-                int ks = 0;
-                int ksAnuls = 0;
-
-                for (int k = 1; k < lines.Length; k++)
-                {
-                    string[] fields = lines[k].Split(';');
-
-                    if (fields[0].CompareTo("KS") == 0)
-                    {
-                        if (fields[5].CompareTo("95") == 0)
-                        {
-                            annuls.Add(fields[6]);
-                        }
-                    }
-                }
+                int kontoAfsteminger = 0;
 
                 for (int k = 1; k < lines.Length; k++)
                 {
@@ -61,19 +45,20 @@ namespace Converter
 
                     if (fields[0].CompareTo("KS") == 0)
                     {
+                        kontoAfsteminger++;
                         ImpRecord impRecord = new ImpRecord(logger);
 
                         // impRecord.setTransactionNumber(fields[4]); use SuperPorts
                         impRecord.setAmount(fields[14]);
                         // take last 14 digits
                         impRecord.setAccountNumber(fields[2], false, 14);
+
+                        numberOfSupoerPortRecords++;
+                        impRecord.writeKonto(fileName);
                     }
                 }
 
-                if (ub > 0) logger.Write("      Udbytte : " + ub);
-                if (kr > 0) logger.Write("      Kupon : " + kr);
-                if (ks > 0) logger.Write("      Køb Salg : " + ks);
-                if (ksAnuls > 0) logger.Write("      Køb Salg, annuleret : " + ksAnuls);
+                if (kontoAfsteminger > 0) logger.Write("      Konto Afsteminger : " + kontoAfsteminger);
             }
 
             return numberOfSupoerPortRecords;
